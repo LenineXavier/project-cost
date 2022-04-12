b9d3ac3853071bb5cc01ea281a23e254d07c626e
Lenine Xavier <leninecxavier@gmail.com>
2022-04-12 17:40:14 -0300

    criando rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-
+import {BrowserRouter, Routes, Route,Link } from "react-router-dom"
+import { Company } from "./components/pages/Company";
+import { Contact } from "./components/pages/Contact";
+import { Home } from "./components/pages/Home";
+import { NewProject } from "./components/pages/NewProject";
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-        
-      
+    <BrowserRouter>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+    
+    
+    
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/company" element={<Company/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="/newproject" element={<NewProject/>} />
+    </Routes>
+
+    <div>
+      <p>Footer</p>
     </div>
+    </BrowserRouter>
   );
 }
 
